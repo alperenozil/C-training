50ce331cf958c33384bacfe860e83326b991be3f
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-20 21:13:51 +0300

    Create strtok-ex.c

---
+++ strtok-ex.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <string.h> 
+  
+int main() 
+{ 
+    char str[] = "Necdet-Alperen-Ozil-Fenerbahce-Adana-iOS-Android"; 
+  
+    // Returns first token 
+    char* word = strtok(str, "-"); 
+
+    // Prints all words 
+    while (word != NULL) { 
+        printf("%s\n", token); 
+        word = strtok(NULL, "-"); 
+    } 
+    return 0; 
+} 
